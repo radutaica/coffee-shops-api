74b15ed5cef836c5208d2782ac060c665d2f50c6
radutaica <radu.andrei.taica@gmail.com>
2026-04-03 13:03:44 +0300

    feat: add name filter to GraphQL coffeeShops query with specs, add pry gem for debugging

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,14 +1,17 @@
 # frozen_string_literal: true
+require "pry"
 
 module Types
   class QueryType < Types::BaseObject
     field :coffee_shops, [ Types::CoffeeShop ], null: false do
       argument :y, Float, required: true
       argument :x, Float, required: true
+      argument :name, String, required: false, default_value: nil
     end
 
-    def coffee_shops(x:, y:)
+    def coffee_shops(x:, y:, name:)
       shops = ::CoffeeShop.all
+      shops = shops.where("name LIKE ?", "%#{name}%") if name.present?
       CoffeeShopFinder.new(x: x, y: y, shops: shops).call
     end
   end
