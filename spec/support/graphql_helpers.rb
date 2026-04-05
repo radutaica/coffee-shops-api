dfd155a2f212e3949291d2cf537a1727c98044a9
radutaica <radu.andrei.taica@gmail.com>
2026-04-05 21:31:20 +0300

    test: add GraphQL test helpers and configure test API key
    Add shared graphql_headers helper module, auto-load spec/support files,
    set test API key env var, and include helpers in request specs.

---
+++ spec/support/graphql_helpers.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module GraphqlHelpers
+  def graphql_headers(api_key: ENV["GRAPHQL_API_KEY"])
+    {
+      "Content-Type" => "application/json",
+      "Authorization" => "Bearer #{api_key}"
+    }
+  end
+
+  def graphql_headers_without_auth
+    { "Content-Type" => "application/json" }
+  end
+end
