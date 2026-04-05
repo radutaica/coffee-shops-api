28b376b66d900deaf5d51ebb1cf34ef6e4386316
radutaica <radu.andrei.taica@gmail.com>
2026-04-05 21:56:54 +0300

    fix: use credentials as primary API key source in test helper
    The test helper was using ENV["GRAPHQL_API_KEY"] which is nil in test,
    while the controller checks Rails credentials first. Align both to use
    the same key resolution order.

--- spec/support/graphql_helpers.rb
+++ spec/support/graphql_helpers.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module GraphqlHelpers
-  def graphql_headers(api_key: ENV["GRAPHQL_API_KEY"])
+  def graphql_headers(api_key: Rails.application.credentials.dig(:graphql, :api_key) || ENV["GRAPHQL_API_KEY"])
     {
       "Content-Type" => "application/json",
       "Authorization" => "Bearer #{api_key}"
